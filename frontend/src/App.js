d1f9a1134afdbf7c26bb27213b147198a353c44a
Rahul Pavan <100563831+rahulpavan-21@users.noreply.github.com>
2026-07-04 13:26:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ import CursorOrb from "./components/site/CursorOrb";
 function App() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.15,
+      duration: 0.7,
+      wheelMultiplier: 1.2, 
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     });
